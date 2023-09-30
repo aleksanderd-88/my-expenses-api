54d07a100a45e67eba3c1c42c9296b869fdcae41
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-09-30 18:22:58 +0200

    Implement business logic for expense controllers

--- src/controllers/api/v1/expenses/create.js
+++ src/controllers/api/v1/expenses/create.js
@@ -1,1 +1,14 @@
-module.exports = (req, res) => {}
+const models = require('../../../../models')
+
+module.exports = (req, res) => {
+  const data = req.body.data
+
+  // Sanity check
+  if ( !data || !Object.keys(data).length || Object.values(data).some(o => o === '') )
+    return res.status(400).send(`Can't continue with request`)
+
+  // Make the query
+  return models.Expense.create(data)
+    .then(() => res.status(201).send('Expense created'))
+    .catch(err => res.status(500).send(`Error: ${ err }`))
+}
--- src/controllers/api/v1/expenses/list.js
+++ src/controllers/api/v1/expenses/list.js
@@ -1,1 +1,8 @@
-module.exports = (req, res) => {}
+const models = require('../../../../models')
+
+// Make the query
+module.exports = (req, res) => {
+  return models.Expense.find({}).lean()
+    .then((results) => res.status(200).send({ rows: results }))
+    .catch(err => res.status(500).send(`Error: ${ err }`))
+}
