3f0c98df4bd73dd4a1d907c95121f7c60d063d68
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-10-01 23:11:05 +0200

    Implement pagination - WIP

--- src/controllers/api/v1/expenses/list.js
+++ src/controllers/api/v1/expenses/list.js
@@ -2,7 +2,34 @@ const models = require('../../../../models')
 
 // Make the query
 module.exports = (req, res) => {
-  return models.Expense.find({}).lean()
-    .then((results) => res.status(200).send({ rows: results }))
+  const data = req.body.data
+
+  // Sanity check
+  if ( !data || !Object.keys(data).length || Object.values(data).some(o => o === '') )
+    return res.status(400).send(`Can't continue with request`)
+
+  console.log(data);
+
+  const limit = Number(data.limit || 10)
+  const perPage = Number(data.perPage || 10)
+  const page = Number(data.page || 1)
+  const offset = Number(data.offset || 10)
+  const sort = data.sort || 'id'
+  
+  return models.Expense.find({})
+    .limit(limit)
+    .skip(page * limit)
+    .sort(sort)
+    .lean()
+    .then( async (results) => {
+
+      const response = {
+        rows: results,
+        count: await models.Expense.estimatedDocumentCount(),
+        page: page,
+        nextPage: page + 1
+      }
+      res.status(200).send(response)
+    })
     .catch(err => res.status(500).send(`Error: ${ err }`))
 }
