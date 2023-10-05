286639b4c9d7a10732575618b13bad164e2292fa
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-10-05 20:37:00 +0200

    Change status code

--- src/controllers/api/v1/income/update.js
+++ src/controllers/api/v1/income/update.js
@@ -9,6 +9,6 @@ module.exports = (req, res) => {
 
   // Make the query
   return models.Income.findOneAndUpdate({ amount: { $gt: 0 }}, data, { upsert: true })
-    .then(() => res.status(201).send(`Income updated`))
+    .then(() => res.status(200).send(`Income updated`))
     .catch(err => res.status(500).send(`Error: ${ err }`))
 }
