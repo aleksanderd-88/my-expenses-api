d14e78890cdb7ff93186bfaf79e6224f954b4f16
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-10-07 16:41:54 +0200

    Fetch expenses for current month

--- src/controllers/api/v1/expenses/list.js
+++ src/controllers/api/v1/expenses/list.js
@@ -1,8 +1,10 @@
 const models = require('../../../../models')
+const Sugar = require('sugar')
 
 // Make the query
 module.exports = (req, res) => {
-  return models.Expense.find({}).lean()
+  const today = Sugar.Date(new Date()).raw
+  return models.Expense.find({ paymentDue: { $gte: Sugar.Date(new Date(today)).raw} }).lean()
     .then((results) => res.status(200).send({ rows: results }))
     .catch(err => res.status(500).send(`Error: ${ err }`))
 }
