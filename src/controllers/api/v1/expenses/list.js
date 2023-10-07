db2ce800fe8aac5ea240aad549e7f13d3cbea416
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-10-07 23:31:59 +0200

    Revert changes

--- src/controllers/api/v1/expenses/list.js
+++ src/controllers/api/v1/expenses/list.js
@@ -1,10 +1,8 @@
 const models = require('../../../../models')
-const Sugar = require('sugar')
 
 // Make the query
 module.exports = (req, res) => {
-  const today = Sugar.Date(new Date()).raw
-  return models.Expense.find({ paymentDue: { $gte: Sugar.Date(new Date(today)).raw} }).lean()
+  return models.Expense.find({}).lean()
     .then((results) => res.status(200).send({ rows: results }))
     .catch(err => res.status(500).send(`Error: ${ err }`))
 }
