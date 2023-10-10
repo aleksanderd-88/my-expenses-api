8008381f8d74873a50e8b7a12ba23994e029e564
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-10-10 19:56:37 +0200

    Implement copy previous month functionality

--- src/controllers/api/v1/expenses/create.js
+++ src/controllers/api/v1/expenses/create.js
@@ -1,11 +1,42 @@
 const models = require('../../../../models')
+const Sugar = require('sugar')
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const data = req.body.data
 
   // Sanity check
   if ( !data || !Object.keys(data).length || Object.values(data).some(o => o === '') )
     return res.status(400).send(`Can't continue with request`)
+
+  if ( data.copyPrevious ) {
+    if ( !data.date )
+      return res.status(404).send('No date parameter was given')
+
+    const beginningOfMonth = Sugar.Date(data.date).beginningOfMonth().raw
+    const endOfMonth = Sugar.Date(data.date).endOfMonth().raw
+
+    console.log(beginningOfMonth);
+    console.log(endOfMonth);
+    console.log('data.currentExpenseDate', data.currentExpenseDate);
+
+    const expenses = await models.Expense.find({ paymentDue: { $gte: beginningOfMonth, $lte: endOfMonth } }).lean()
+    if ( !expenses.length )
+      return res.status(404).send('No previous expenses at given date has been found')
+
+    const resetExpenses = expenses.map(e => {
+      return {
+        name: e.name || '',
+        cost: e.cost || 0,
+        isPaid: false,
+        paidAt: null,
+        paymentDue: Sugar.Date(e.paymentDue).addMonths(1).raw
+      }
+    })
+
+    return models.Expense.insertMany(resetExpenses)
+      .then(() => res.status(201).send('Successfully copied previous month'))
+      .catch(err => res.status(500).send(`Error: ${ err }`))
+  }
 
   // Make the query
   return models.Expense.create(data)
