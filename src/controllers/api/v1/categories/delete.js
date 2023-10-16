a8aff0983916400da0e6123827632c4fd8918053
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-10-16 09:48:55 +0200

    Set expense categoryId field to null when deleting category

--- src/controllers/api/v1/categories/delete.js
+++ src/controllers/api/v1/categories/delete.js
@@ -9,6 +9,9 @@ module.exports = (req, res) => {
 
   // Make the query
   return models.Category.deleteOne({ _id: id })
-    .then((deleted) => res.status(200).send({ deletedRows: deleted['deletedCount']}))
+    .then( async (deleted) => {
+      await models.Expense.updateMany({ categoryId: id }, { categoryId: null })
+      res.status(200).send({ deletedRows: deleted['deletedCount']})
+    })
     .catch(err => res.status(500).send(`Error: ${ err }`))
 }
