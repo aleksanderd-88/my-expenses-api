fe5999ebd2bb75ddf75d16b2b7107cd17e156cab
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-10-24 22:58:44 +0200

    Modify queries

--- src/controllers/api/v1/income/get.js
+++ src/controllers/api/v1/income/get.js
@@ -3,7 +3,7 @@ const models = require('../../../../models')
 module.exports = (req, res) => {
 
   // Make the query
-  return models.Income.findOne({ amount: { $gt: 0 }, userId: req.user._id }).lean()
+  return models.Income.findOne({ name: { $eq: null }, userId: req.user._id }).lean()
     .then((income) => res.status(200).send(income ? income : {}))
     .catch(err => res.status(500).send(`Error: ${ err }`))
 }
--- src/controllers/api/v1/income/update.js
+++ src/controllers/api/v1/income/update.js
@@ -12,7 +12,7 @@ module.exports = (req, res) => {
     data.userId = req.user._id
 
   // Make the query
-  return models.Income.findOneAndUpdate({ amount: { $gt: 0 }, userId: data.userId }, data, { upsert: true })
+  return models.Income.findOneAndUpdate({ name: { $eq: null }, amount: { $gt: 0 }, userId: data.userId }, data, { upsert: true })
     .then(() => res.status(200).send(`Income updated`))
     .catch(err => res.status(500).send(`Error: ${ err }`))
 }
